168f4f0d7fe0749453484cb18c6ccfce3b1b4b62
zebrowskijakub <jakub.zebrowski@infopark.de>
2017-12-05 16:54:09 +0100

    changed all tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,32 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
 
   test "should get home" do
-    get :home
+    get static_pages_home_path
     assert_response :success
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get :help
+    get static_pages_help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get :about
+    get static_pages_about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
-    get :contact
+    get static_pages_contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
